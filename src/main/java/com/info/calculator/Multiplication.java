fe21eacb5a7fa3e223f5bdae3090fd4290527c70
Vikash Padit <vikashpadit841245@gmail.com>
2026-06-28 18:53:19 +0530

    Initial commit - calculator project

---
+++ src/main/java/com/info/calculator/Multiplication.java
@@ -0,0 +1,8 @@
+package com.info.calculator;
+
+public class Multiplication {
+    public int multiply(int a , int b){
+        return a*b;
+    }
+
+}
---
+++ src/main/java/com/info/calculator/Subtraction.java
@@ -0,0 +1,7 @@
+package com.info.calculator;
+
+public class Subtraction {
+    public int sub(int a, int b){
+        return a-b;
+    }
+}
